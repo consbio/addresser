66d0c4fe4e053d61701bad71098765c3b6f8577f
Nikolas Stevenson-Molnar <nik.molnar@consbio.org>
2020-02-12 19:49:23 -0800

    Copy README text to `long_description` for PyPI

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
+import os
+
 from setuptools import setup
 
 import addresser
 
+with open(os.path.join(os.path.dirname(__file__), 'README.rst'), 'r') as f:
+    long_description = f.read()
+
 setup(
     name="addresser",
     description="A Python port of the `parse-address` npm library",
-    long_description="A Python port of the `parse-address` npm library",
+    long_description=long_description,
     long_description_content_type="text/x-rst",
     keywords="address parser normalizer usa us parse-address",
     version=addresser.__version__,
